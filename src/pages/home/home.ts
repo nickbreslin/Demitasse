109f4b001abe3e721907835ea95b35ca7ca449b2
Nick Breslin <nickbreslin@gmail.com>
2016-10-26 21:54:13 -0700

    Basic theming

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,6 +8,11 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public event = {
+    month: '1990-02-19',
+    timeStarts: '07:43',
+    timeEnds: '1990-02-20'
+  }
   constructor(public navCtrl: NavController) {
 
   }
