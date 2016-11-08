54e2af8e295d87017c2faa50d5c64a6c090f1a5c
Nick Breslin <nickbreslin@gmail.com>
2016-11-07 19:58:32 -0800

    Updating tabs with new pages

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
-import { HomePage }    from '../home/home';
-import { AboutPage }   from '../about/about';
-import { RoastsPage }  from '../roasts/roasts';
+import { HomePage    } from '../home/home';
+import { HistoryPage } from '../history/history';
+import { RoastsPage  } from '../roasts/roasts';
+import { MapPage     } from '../map/map';
+import { StatsPage   } from '../stats/stats';
+
 
 
 @Component({
@@ -12,10 +15,10 @@ export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
   tab1Root: any = HomePage;
-  tab2Root: any = AboutPage;
+  tab2Root: any = HistoryPage;
   tab3Root: any = RoastsPage;
-  tab4Root: any = RoastsPage;
-  tab5Root: any = RoastsPage;
+  tab4Root: any = MapPage;
+  tab5Root: any = StatsPage;
 
   constructor() {
 
