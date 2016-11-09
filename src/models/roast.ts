a39a1043b91cadc09cc97d71df67205a8d78872e
Nick Breslin <nickbreslin@gmail.com>
2016-11-08 22:05:42 -0800

    Roast model

---
+++ src/models/roast.ts
@@ -0,0 +1,26 @@
+export class RoastModel {
+	
+	// Properties
+	/*
+		count
+		
+		roaster (company)
+
+		created
+
+		start
+		finished
+
+		rating
+	*/
+
+	constructor() {
+		console.log('created');
+	}
+
+	// Write / Save
+
+
+	// Read / Load
+
+}
