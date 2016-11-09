3336230e30ba8756497076483d3ccd8b15716447
Nick Breslin <nickbreslin@gmail.com>
2016-11-08 22:05:17 -0800

    Cup Model

--- src/models/cup.ts
+++ src/models/cup.ts
@@ -1,7 +1,36 @@
 export class CupModel {
 	
+	// Properties
+	/*
+		size (oz)
+		size (category)
+		
+		roast
+
+		created
+
+		start
+		finished
+
+		rating
+	*/
+
+	private rating;
+
 	constructor() {
 		console.log('created');
+		//this.created = 
 	}
+
+	setRating(rating) {
+		this.rating = rating;
+	}
+
+
+
+	// Write / Save
+
+
+	// Read / Load
 
 }
